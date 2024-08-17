33257637d22958922c5fef1ac1ec5158a77cb094
1NS <ins16mail@gmail.com>
2024-08-17 14:08:07 +0300

    Update gpsoff.lua
    - add hotkey on G key

--- arizona/gpsoff.lua
+++ arizona/gpsoff.lua
@@ -4,11 +4,20 @@ script_url("https://github.com/ins1x/moonloader-scripts")
 script_author("1NS")
 -- moonloader 0.26+
 
+-- Activation: type cmd /gpsoff or press key G on vehicle (only for driver)
 function main()
    if not isSampLoaded() or not isSampfuncsLoaded() then return end
    while not isSampAvailable() do wait(100) end
    sampRegisterChatCommand("gpsoff", disableCheckpoint)
-   wait(-1)
+   --wait(-1)
+   while true do 
+      if isKeyDown(0x47) and isCharInAnyCar(PLAYER_PED) then
+         if getDriverOfCar(getCarCharIsUsing(PLAYER_PED)) ~= -1 then
+            disableCheckpoint()
+         end
+      end
+      wait(0) 
+   end
 end
 
 function disableCheckpoint()
