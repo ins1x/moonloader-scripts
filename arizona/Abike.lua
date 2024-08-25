2b21f4850a3a3234ca5abdec206d020b043d2fbe
1NS <ins16mail@gmail.com>
2024-08-25 20:03:07 +0300

    Update Abike.lua
    fixed excessive acceleration at startup

--- arizona/Abike.lua
+++ arizona/Abike.lua
@@ -1,9 +1,10 @@
 script_name("Abike")
 script_description("Pressing W on the bike or skate automatically accelerates it")
 script_url("https://github.com/ins1x/moonloader-scripts")
-script_version("0.1")
+script_version("0.2")
 
 local bikeModel = {[481] = true, [509] = true, [510] = true, [15882] = true}
+local accelDelay = 200
 
 function main()
    if not isSampLoaded() or not isSampfuncsLoaded() then return end
@@ -14,10 +15,15 @@ function main()
       if not sampIsChatInputActive() and not sampIsDialogActive() and not isSampfuncsConsoleActive() then
          if isCharOnAnyBike(playerPed) and isKeyDown(0x57) then
             if bikeModel[getCarModel(storeCarCharIsInNoSave(playerPed))] then
+               if accelDelay > 10 then 
+                  accelDelay = accelDelay - 10
+               end
                setGameKeyState(16, 255)
-               wait(10)
+               wait(accelDelay)
                setGameKeyState(16, 0)
             end
+         else
+            accelDelay = 200
          end
       end
    end
