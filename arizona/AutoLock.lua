7df08fb66cf59797c541daa40621af3a77f317f1
1NS <ins16mail@gmail.com>
2024-08-31 10:07:53 +0300

    Update AutoLock.lua
    remove lib.samp.events dependencies

--- arizona/AutoLock.lua
+++ arizona/AutoLock.lua
@@ -1,46 +1,48 @@
 script_name("AutoLock")
 script_description("Auto lock vehicle")
+-- Automatically locks your vehicle, and automatically 
+-- opens locked vehicles when you press F or ENTER
 script_url("https://github.com/ins1x/moonloader-scripts")
-script_dependencies('lib.samp.events')
-script_version("0.2")
+script_version("0.3")
 script_author("1NS")
 -- encoding.default = 'CP1251'
 -- script_moonloader(16) moonloader v.0.26
--- simple autolock for my own needs
-local sampev = require 'lib.samp.events'
+-- activation: auto
 
-local lastVehicleId = nil
+local lastVehicleHandle = nil
 
 function main()
    if not isSampLoaded() or not isSampfuncsLoaded() then return end
    while not isSampAvailable() do wait(100) end
    while true do
       wait(0)
+      if isCharInAnyCar(playerPed) then 
+         lastVehicleHandle = storeCarCharIsInNoSave(playerPed)
+      end
       if isKeyDown(0x0D) or isKeyDown(0x46) then -- Enter/F
          if not sampIsChatInputActive() and not sampIsDialogActive() then
-            if lastVehicleId then
-               local result, car = sampGetCarHandleBySampVehicleId(lastVehicleId)
-               local doorStatus = getCarDoorLockStatus(car)
-               local engineState = isCarEngineOn(car)
-               if doorStatus > 0 then -- if doors locked unlock them
+            if isCharInAnyCar(playerPed) then
+               local currentcarhandle = storeCarCharIsInNoSave(playerPed)
+               local currentcarDoorStatus = getCarDoorLockStatus(currentcarhandle)
+               if currentcarDoorStatus ~= 2 then 
                   wait(500)
                   sampSendChat("/lock")
+               end
+            else
+               if lastVehicleHandle then
+                 local doorStatus = getCarDoorLockStatus(lastVehicleHandle)
+                 if doorStatus > 0 then -- if doors locked unlock them
+                    wait(500)
+                    sampSendChat("/lock")
+                 end
                end
             end
          end
       end
-      
-      -- L key
-      -- if isKeyJustPressed(76) and not sampIsChatInputActive() and not sampIsDialogActive() then
+      -- L key bind to /lock vehicle
+      --if isKeyJustPressed(76) and not sampIsChatInputActive() and not sampIsDialogActive() then
          --sampSendChat("/lock")
       --end
    end
 end
 
-function sampev.onSendExitVehicle(vehicleId)
-   lastVehicleId = vehicleId
-   lua_thread.create(function()
-      wait(500)
-      sampSendChat("/lock")
-   end)
-end
