f440afda035d225fd628430cf4760888cdc3bd9f
1NS <ins16mail@gmail.com>
2024-09-09 20:07:16 +0300

    Update AutoLock.lua
    new more responsible algoritm

--- arizona/AutoLock.lua
+++ arizona/AutoLock.lua
@@ -3,22 +3,17 @@ script_description("Auto lock vehicle")
 -- Automatically locks your vehicle, and automatically 
 -- opens locked vehicles when you press F or ENTER
 script_url("https://github.com/ins1x/moonloader-scripts")
-script_version("0.3")
+script_version("0.4")
 script_author("1NS")
 -- encoding.default = 'CP1251'
 -- script_moonloader(16) moonloader v.0.26
 -- activation: auto
-
-local lastVehicleHandle = nil
 
 function main()
    if not isSampLoaded() or not isSampfuncsLoaded() then return end
    while not isSampAvailable() do wait(100) end
    while true do
       wait(0)
-      if isCharInAnyCar(playerPed) then 
-         lastVehicleHandle = storeCarCharIsInNoSave(playerPed)
-      end
       if isKeyDown(0x0D) or isKeyDown(0x46) then -- Enter/F
          if not sampIsChatInputActive() and not sampIsDialogActive() then
             if isCharInAnyCar(playerPed) then
@@ -29,12 +24,11 @@ function main()
                   sampSendChat("/lock")
                end
             else
-               if lastVehicleHandle then
-                 local doorStatus = getCarDoorLockStatus(lastVehicleHandle)
-                 if doorStatus > 0 then -- if doors locked unlock them
-                    wait(500)
-                    sampSendChat("/lock")
-                 end
+               -- Will work on vehicles within a radius of 3 meters
+               local closestcarhandle, closestcarid = getClosestCar(3)
+               if closestcarhandle then
+                  wait(1000) 
+                  sampSendChat("/lock")   
                end
             end
          end
@@ -44,5 +38,25 @@ function main()
          --sampSendChat("/lock")
       --end
    end
+end
+
+function getClosestCar(minDist)
+   -- TIP: return 2 values: car handle and car id
+   local closestId = -1
+   local closestHandle = false
+   local x, y, z = getCharCoordinates(playerPed)
+   for i, k in ipairs(getAllVehicles()) do
+      local streamed, carId = sampGetVehicleIdByCarHandle(k)
+      if streamed then
+         local xi, yi, zi = getCarCoordinates(k)
+         local dist = math.sqrt( (xi - x) ^ 2 + (yi - y) ^ 2 + (zi - z) ^ 2 )
+         if dist < minDist then
+            minDist = dist
+            closestId = carId
+            closestHandle = k
+         end
+      end
+   end
+   return closestHandle, closestId
 end
 
